4035b4e097570b7c2d494cb42715ed0f7efc81a0
Janice Dunn & Lisa Folliard <student@epicodus.com>
2015-09-16 10:01:22 -0700

    return the player's mark with x and y coordinates.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 function Player(mark) {
   this.mark = mark;
 }
+
+function Space(xCoordinate, yCoordinate) {
+  this.xCoordinate = xCoordinate;
+  this.yCoordinate = yCoordinate;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -3,5 +3,17 @@ describe('Player', function() {
     var testPlayer = new Player("X");
     expect(testPlayer.mark).to.equal("X");
   });
+});
+
+describe('Space', function() {
+  it("returns the player's mark", function() {
+    var testSpace = new Space(1, 2);
+    expect(testSpace.xCoordinate).to.equal(1);
+  });
+
+  it("returns the player's mark", function() {
+    var testSpace = new Space(1, 2);
+    expect(testSpace.yCoordinate).to.equal(2);
+  });
 
 });
